7bede8b43f517717215fd565216e7c9685e513d9
James Cheng <jacheng@mozilla.com>
2015-05-12 18:12:39 +0800

    add comment

--- TLS.cpp
+++ TLS.cpp
@@ -3,17 +3,24 @@
 #include <cstdlib>
 #include <unistd.h>
 using namespace std;
+//Build command.
 //g++ -o test TLS.cpp -std=c++0x -lpthread
 //g++ -o test TLS.cpp -std=c++0x -lpthread -m32
+//Key Idea:
+//用一個global 的key 去跟linux 要value, 而key會因為不同的thread而mapping到不一樣的value.
+//這class能夠吃 sizeof < pointer的物件, 若 > pointer size , 請存pointer
+
 typedef void* (*ThreadProc)(void*);
 ThreadLocal<long long> gTLS;
 void thread(void*)
 {
+  int count = 0;
+  gTLS.set(count);
   for(;;)
   {
     usleep(200000);
     printf("[%u] get = %llx\n", (unsigned int)pthread_self(), gTLS.get());
-
+    gTLS.set(++count);
   }
 }
 
