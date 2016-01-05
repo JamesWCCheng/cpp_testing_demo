438304782be1c3592909329308ce0073209233aa
James Cheng <jacheng@mozilla.com>
2016-01-05 18:29:48 +0800

    add tls

--- std_async.cpp
+++ std_async.cpp
@@ -3,9 +3,13 @@
 #include <thread>
 #include <future>
 #include <chrono>
+#include <vector>
 using namespace std;
+thread_local int tls = 0;
 int Task(int a, int b)
 {
+  cout << "tls = " << tls << endl;
+  tls++;
   std::thread::id this_id = std::this_thread::get_id();
   cout << "Task ++ ID= " << this_id << " a = " << a << ", b = " << b << endl;
   std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -22,7 +26,7 @@ int main()
   auto async = std::launch::async;
   // 跑在呼叫get or wait的thread
   auto deferred = std::launch::deferred;
-  // 行為很怪異,
+  // 行為很怪異, 
   // g++, 行為跟deferred 一樣
   // VC 行為跟async 一樣
   auto async_deferred = std::launch::async | std::launch::deferred;
@@ -49,7 +53,7 @@ int main()
   {
     cout << "catch = " << err << endl;
   }
-  // Task  有exception 他會先幫你catch, 如果你去get()就rethrow
+  // hreadProc  有exception 他會先幫你catch, 如果你很靠北去get()就rethrow~~
   getchar();
   return 0;
 }
